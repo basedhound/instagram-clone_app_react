d2232f5912090ac35e10ce2840507f569f092f5f
Jozef S <franck.vukelic@gmail.com>
2024-05-26 17:11:57 +0700

    Add: appwrite config + api (createUser), types file

---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,33 @@
+import { ID, Query } from "appwrite";
+//
+import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
+import { account } from "./config";
+
+// Auth
+export async function createUserAccount(user: INewUser) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      user.email,
+      user.password,
+      user.name
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(user.name);
+
+    const newUser = await saveUserToDB({
+      accountId: newAccount.$id,
+      name: newAccount.name,
+      email: newAccount.email,
+      username: user.username,
+      imageUrl: avatarUrl,
+    });
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+}
